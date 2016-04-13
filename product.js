54cc8410176bda0a0d182a4baeb55c9571419658
Megan Ford <meg.c.ford@gmail.com>
2016-04-13 13:04:32 -0500

    markup created, initial selector variables written

--- product.js
+++ product.js
@@ -2,50 +2,60 @@ var planet1 = {
  name = "Fuzzy Planet"
  image = "<img src = "img/fuzzyPlanet.jpg" alt = "Tribble Planet"></img>"
  description = "Enjoy soft, cute, plush and squishy EVERYTHING? Don't mind fur in your bathtub? This is the place for you! Approximately 75 miles in diameter with 85% earth gravity, bounce and snuggle your way into this adorable planet today!"
- price = "$800.00 plus S&H"
+ price = "$800.00"
 }
 
 var planet2 = {
  name = "Gas Giant"
  image = "img src = "img/gasPlanet.jpg" alt = "Gas Planet"></img>"
  description = "Are you enormous? Made of clouds? Impervious to gale force winds, extreme temperatures, and gravity hundreds of times that of Earth's? Look no further- this gas giant has got it all! Build yourself an igloo made of solid oxygen and settle down in your very own permanent maelstrom today!"
- price = "$40 plus S&H"
+ price = "$40"
 }
 var planet3 = {
  name = "Trojan Planet"
  image = "img src = "img/trojanPlanet.jpg" alt = "Trojan Planet"></img>"
  description = "Hypochondriacs rejoice! This extremely well-protected planet is impervious to almost any disease and also includes a hypoallergenic asteroid barrier. Available in sizes Large, Extra Large, Enormous, and Too Big, and ribbed for your pleasure."
- price = "$300 plus S&H"
+ price = "$300"
 }
 var planet4 = {
  name = "Ice Planet"
  image = "<img src = "img/icePlanet.jpg" alt = "Ice Planet"></img>"
  description ="Perfect for the jilted lover, yeti, or Elsa, this gorgous planet may be lacking in coziness (see <a href="#">'fuzzy planet'</a>), but features a gorgeous landscape of icicles, snowdrifts, and boulders perfect for the budding ice-architect looking to move beyond the solid-oxygen igloo."
- price = "$60000 plus S&H"
+ price = "$60000"
 }
 var planet5 = {
  name = "Aqua Planet"
  image = "<img src = "img/aquaPlanet.png" alt = "Aqua Planet"></img>"
  description = "The perfect planet for fish, orcas, and really, really strong swimmers. Seafood lovers, surfers, and otters encouraged as well. BYOB(oat) to this lovely habitat teeming with gorgous marine life and seagulls. Bask in a variety of weather events from light rain to hurricane, and don't worry about staying DRY!"
- price = "$2 million plus S&H"
+ price = "$2 million"
 }
 var planet6 = {
  name = "Helium Planet"
  image = "<img src = "img/heliumPlanet.jpg" alt = "Helium Planet"></img>"
  description = "An atmosphere made of helium means you and all your visitors will sport Silly Voices while on this planet! Take it a step further by furnishing with all your favorite memes, dad jokes, and hilarious gifs. Gravity 40% of earth's surface means even falling down is more funny than painful!"
- price = "$415 plus S&H"
+ price = "$415"
 }
 var planet7 = {
  name = "Desert Planet"
  image = "<img src = "img/desertPlanet.jpg" alt = "Desert Planet"></img>"
  description = "Pack your sunscreen and an cause this planet is HOT! A great spot for camel-riding, pyramid-building, and podracing, this deserted desert gives you the opportunity to start fresh with a sandbox of your very own! *Water Not Included"
- price = "$969 plus S&H"
+ price = "$969"
 }
 var planet8 = {
  name = "<h2>Pluto</h2>"
  image = "<img src = "img/pluto.png" alt = "Pluto"></img>"
  description = "Not technically a planet, this small space body is a great option for the budget-minded planet purchaser. Large heart-shaped landscape feature pre-added, because Pluto Loves You!"
- price = "FREE plus S&H"
+ price = "FREE"
 }
 
 var planetArray = [planet1, planet2, planet3, planet4, planet5, planet6, planet7, planet8];
+
+console.log planetArray[planet2].description;
+
+var titleReplace = document.getElementById("card1").getElementsByClassName("cardTitle");
+
+var imageReplace = document.getElementById("card1").getElementsByClassName("cardImage");
+
+var descriptionReplace = document.getElementById("card1").getElementsByClassName("cardDescription");
+
+var priceReplace = document.getElementById("card1").getElementsByClassName("cardPrice")
