dd20e96a6318345d3fb5cf83f5d9f3915d905725
Megan Ford <meg.c.ford@gmail.com>
2016-04-13 10:17:15 -0500

    object structure set up. first three planets added

---
+++ product.js
@@ -0,0 +1,50 @@
+var planet1 = {
+ name = "<h2>Fuzzy Planet</h2>"
+ image = "<img src = "img/planetTribble.jpeg" alt = "Tribble Planet"></img>"
+ description = "<p>Enjoy soft, cute, plush and squishy EVERYTHING? Don't mind fur in your bathtub? This is the place for you! Approximately 75 miles in diameter with 85% earth gravity, bounce and snuggle your way into this adorable planet today!</p>"
+ price = "<p>$800.00 plus S&H</p>"
+}
+
+var planet2 = {
+ name = "<h2>Gas Giant</h2>"
+ image = "img src = "img/gasGiantPlanet.jpeg" alt = "Gas Giant Planet"></img>"
+ description = "<p>Are you enormous? Made of clouds? Impervious to gale force winds, extreme temperatures, and gravity hundreds of times that of Earth's? Look no further- this gas giant has got it all! Build yourself an igloo made of solid oxygen and settle down in your very own permanent maelstrom today!</p>"
+ price = "<p>$40 plus S&H</p>"
+}
+var planet3 = {
+ name = "<h2>Trojan Planet</h2>"
+ image = "img src = "img/trojanPlanet.jpeg" alt = "Trojan Planet"></img>"
+ description = "Hypochondriacs rejoice! This extremely well-protected planet is impervious to almost any disease and also includes a hypoallergenic asteroid barrier. Available in sizes Large, Extra Large, Enormous, and Too Big, and ribbed for your pleasure."
+ price = "$300 plus S&H"
+}
+var planet4 = {
+ name = "<h2>Ice Giant</h2>"
+ image =
+ description =
+ price =
+}
+var planet5 = {
+ name = 
+ image =
+ description =
+ price =
+}
+var planet6 = {
+ name = 
+ image =
+ description =
+ price =
+}
+var planet7 = {
+ name = 
+ image =
+ description =
+ price =
+}
+var planet8 = {
+ name = 
+ image =
+ description =
+ price =
+}
+// name, description, price, and product image
